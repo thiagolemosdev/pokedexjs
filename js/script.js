c6d92667802df99587ece75969abe043e14c7b7a
Thiago Lemos <lemoos.th@gmail.com>
2022-08-13 16:56:13 +0000

    Fetch funcional

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const pokemonNumber = document.querySelector(".pokemon__number");
+const pokemonName = document.querySelector(".pokemon__name");
+const pokemonImage = document.querySelector(".pokemon__image");
+const searchPokemon = document.querySelector(".input__search");
+const form = document.querySelector(".form");
+// pegando os elementos do DOM que serao alterados
+
 const fetchPokemon = async (pokemon) => {
   // Criando a função para buscar o pokemon
   // Aqui usamos Async porque o fecth é uma funçao assincrona (uma promisse)
@@ -7,8 +14,8 @@ const fetchPokemon = async (pokemon) => {
     // Atribuindo o valor de resposta do fecth a variavel
     // utilizamos o await para informar que ele precisa esperar a conclusão para continuar
 
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-    // Passando o endereço do fetch
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
+    // Passando o endereço do fetch e passando a variavel e minusculo
   );
 
   const data = await APIResponse.json();
@@ -19,7 +26,26 @@ const fetchPokemon = async (pokemon) => {
 };
 
 const renderPokemon = async (pokemon) => {
+  // criando função para renderizar os dados do pokemon
+
   const data = await fetchPokemon(pokemon);
+  // Variavel para receber os dados do pokemon
+
+  pokemonNumber.innerHTML = data.id;
+  pokemonName.innerHTML = data.name;
+  // Atribuindo o valor coletado as variaveis e levando para o front
+  pokemonImage.src =
+    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_default"
+    ];
+  // pegando o gif do pokemon, ne caso estamos usando colchete mas podemos usar ponto tambem
+
+  searchPokemon.value = "";
+  // limpando o campo de input
 };
 
-renderPokemon("25");
+form.addEventListener("submit", (e) => {
+  // adicionando
+  e.preventDefault();
+  renderPokemon(searchPokemon.value);
+});
